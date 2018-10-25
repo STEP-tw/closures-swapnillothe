1d9ca5493dc5ebc1abae0bf86c8c4eff62209674
swapnillothe <44019530+swapnillothe@users.noreply.github.com>
2018-10-25 11:17:04 +0530

    Add makeCycler function.
    this function takes one list and return every times next element of that
    list after calling that function.

--- src/lib.js
+++ src/lib.js
@@ -3,9 +3,26 @@ const makeCounterFromN = undefined;
 const makeCounterFromZero = undefined;
 const makeDeltaTracker = undefined;
 const makeFiboGenerator = undefined;
-const makeCycler = undefined;
+//const makeCycler = undefined;
 const curry = undefined;
 const compose = undefined;
+
+const createList = function(listMember){
+  return listMember;
+}
+
+const makeCycler = function(list){
+  let index = 0;
+  let listToCycle = list.map(createList);
+  return cycleList = function(){
+    if(index == (listToCycle.length)){
+      index = 0;
+    }
+   return listToCycle[index++];
+  }
+}
+
+
 
 exports.makeConstant=makeConstant;
 exports.makeCounterFromZero=makeCounterFromZero;
